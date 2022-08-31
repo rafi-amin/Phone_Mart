e7ac7e6cf0210faad8e5df370e9770b3f723ae3c
RAFI KHAN <you@example.com>
2022-08-31 20:35:36 +0600

    entert commit

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ const displayPhones = (phones, dataLimit) =>{
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetils('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
             </div>
          </div>
         `;
@@ -58,8 +59,16 @@ const processSearch = (dataLimit) =>{
 document.getElementById('btn-search').addEventListener('click', function(){
     // start loader
     processSearch(10);
-
 })
+
+//  search input field enter key handler
+
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -77,5 +86,12 @@ document.getElementById('btn-show-all').addEventListener('click' , function(){
     processSearch();
 })
 
+const loadPhoneDetils = async id=>{
+    const url =` https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
 
 // loadPhones();
+
